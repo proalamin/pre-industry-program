619b17c01dbd98062367122ab766d0e509685bdc
proalamin <pro.alamin247@gmail.com>
2026-01-06 12:34:56 +0600

    opinion project -> collect form data use post method

--- Week 4/opinion_project/opinion_app/views.py
+++ Week 4/opinion_project/opinion_app/views.py
@@ -1,11 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
 
 def home(request):
     return render(request, 'home.html')
 
 
 def add_opinion(request):
+    if request.method == 'POST':
+        name = request.POST.get('name')
+        phone = request.POST.get('phone')
+        election_area = request.POST.get('election_area')
+        party = request.POST.get('party')
+
+        
+        return render(request, 'thank_you.html')
+
     return render(request, 'add_opinion.html')
+
+
+def thank_you(request):
+    return render(request, 'thank_you.html')
 
 
 def all_opinions(request):
