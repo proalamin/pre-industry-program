7564f74e98120b25397f72915511c4cff2ca2817
proalamin <pro.alamin247@gmail.com>
2026-01-06 11:11:47 +0600

    opinion project -> templates -> make visual the page also add thank you page

--- Week 4/opinion_project/opinion_app/urls.py
+++ Week 4/opinion_project/opinion_app/urls.py
@@ -5,4 +5,6 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('add-opinion/', views.add_opinion, name='add_opinion'),
     path('all-opinions/', views.all_opinions, name='all_opinions'),
+    path('thank-you/', views.thank_you, name='thank_you'),
+
 ]
